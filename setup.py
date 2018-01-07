0300b8adb59d7023f7861f52f873f21b731a45d1
Troy Dai <troy.dai@outlook.com>
2018-01-06 16:39:22 -0800

    Update build scripts

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-# --------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------------
+#!/usr/bin/env python3
 
 from setuptools import setup
 
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
