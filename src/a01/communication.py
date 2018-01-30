528b12469a41d295ae80793fced15a3cc78e54d5
Troy Dai <troy.dai@outlook.com>
2018-01-30 01:00:18 -0800

    Fix pylint

--- src/a01/communication.py
+++ src/a01/communication.py
@@ -7,7 +7,7 @@ import requests.auth
 from a01.common import TOKEN_FILE, AUTHORITY_URL, CLIENT_ID, RESOURCE_ID
 
 
-class A01Auth(requests.auth.AuthBase):
+class A01Auth(requests.auth.AuthBase):  # pylint: disable=too-few-public-methods
     def __call__(self, req: requests.Request):
         try:
             with open(TOKEN_FILE) as token_file:
@@ -17,8 +17,8 @@ class A01Auth(requests.auth.AuthBase):
                 import adal
                 context = adal.AuthenticationContext(AUTHORITY_URL, api_version=None)
                 access_token = context.acquire_token_with_refresh_token(token['refreshToken'], CLIENT_ID, RESOURCE_ID)
-                for k, v in access_token.items():
-                    token[k] = v
+                for token_key, token_value in access_token.items():
+                    token[token_key] = token_value
                 with open(TOKEN_FILE, 'w') as token_file:
                     token_file.write(json.dumps(token, indent=2))
 
@@ -34,5 +34,5 @@ class A01Auth(requests.auth.AuthBase):
         return req
 
 
-session = requests.Session()
+session = requests.Session()  # pylint: disable=invalid-name
 session.auth = A01Auth()
