6ba8998e274b9dcd1c3d6b48ab3eb11bee7e1d4e
Troy Dai <troy.dai@outlook.com>
2018-03-08 12:50:23 -0800

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,10 +6,10 @@ if [ -z $TRAVIS_TAG ]; then
 fi
 
 if ! command -v az > /dev/null 2>&1; then
-    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-    sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-    sudo apt-get install -y apt-transport-https
-    sudo apt-get -qq update && sudo apt-get install -y azure-cli
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ `lsb_release -cs` main" | tee /etc/apt/sources.list.d/azure-cli.list
+    apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+    apt-get install -y apt-transport-https
+    apt-get -qq update && sudo apt-get install -y azure-cli
 fi
 
 
